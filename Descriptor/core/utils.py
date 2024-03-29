e4bf354873e21ffd03f49b2c93f3cc176f07fa4c
jared <jared@redballoonsecurity.com>
2024-03-28 23:23:38 -0400

    Work directly with component_map, allows linked components

--- Descriptor/core/utils.py
+++ Descriptor/core/utils.py
@@ -1,2 +1,10 @@
 def format_name(input: str):
     return input.replace(':','_').replace(' ','')
+
+def rename_if_duplicate(input: str, in_dict: dict):
+    count = 0
+    new_name = input
+    while in_dict.get(new_name) is not None:
+        new_name = f"{input}_{count}"
+        count += 1
+    return new_name
